d04ffc3d8f6299dad2231e52103c16e08d204916
Syed Ali Raza <92259618+sydalirza@users.noreply.github.com>
2023-12-30 17:22:21 +0500

    added more stuff

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 <script>
 function validateForm() {
-  var x = document.forms["myForm"]["fname"].value;
-  if (x == "") {
-    alert("Name must be filled out");
-    return false;
-  }
+    const firstName = document.forms["query"]["firstname"].value;
+    const lastName = document.forms["query"]["lastname"].value;
+    const email = document.forms["query"]["Email"].value;
+    const query = document.forms["query"]["subject"].value;
+
+    // Basic validation
+    if (firstName === "" || lastName === "" || email === "" || query === "") {
+      alert("All fields must be filled out");
+      return false;
+    }
+
+    // Email validation using a simple regular expression
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      alert("Please enter a valid email address");
+      return false;
+    }
 }
 </script>
